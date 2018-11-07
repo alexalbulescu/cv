e8165a9e3dbdc95ed13fe67c573b7278202faa51
Dru Home Laptop <my_laptop@example.com>
2018-11-07 21:20:32 +0200

    Add last changes

--- send.php
+++ send.php
@@ -6,7 +6,7 @@ $error = "";
 $errorMessage = 'Sorry your message can not be sent.';
 
 //Validate first
-if(empty($name)||empty($email)||empty($message)) 
+if(empty($name)||empty($email)||empty($message))
 {
     echo "Name and email and message are required !";
     header('Location: index.html');
@@ -19,28 +19,28 @@ if(IsInjected($email))
 }
 
 
-$msg =  " Name : $name \r\n"; 
+$msg =  " Name : $name \r\n";
 $msg .= " Email: $email \r\n";
 $msg .= " Message : ".stripslashes($_POST['message'])."\r\n\n";
-$msg .= "User information \r\n"; 
-$msg .= "User IP : ".$_SERVER["REMOTE_ADDR"]."\r\n"; 
-$msg .= "Browser info : ".$_SERVER["HTTP_USER_AGENT"]."\r\n"; 
+$msg .= "User information \r\n";
+$msg .= "User IP : ".$_SERVER["REMOTE_ADDR"]."\r\n";
+$msg .= "Browser info : ".$_SERVER["HTTP_USER_AGENT"]."\r\n";
 $msg .= "User come from : ".$_SERVER["SERVER_NAME"];
 
-$recipient = "me@mail.com";// Change the recipient email adress to your adrees  
+$recipient = "albulescu.victor.alexandru@gmail.com";// Change the recipient email adress to your adrees
 $sujet =  "Sender information";// This is subject you can change it
 $mailheaders = "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\n";
 
 if (!$error){
 
-		$sending = mail($recipient, $sujet, $msg, $mailheaders); 
-		
+		$sending = mail($recipient, $sujet, $msg, $mailheaders);
+
 		if ($sending) {
-				// If the message is sent we output a string to use it 
-				echo "SENDING"; 
+				// If the message is sent we output a string to use it
+				echo "SENDING";
 			} else {
 				// Display Error Message
-				echo $errorMessage; 
+				echo $errorMessage;
 			}
 	} else {
 		echo $error; // Display Error Message
@@ -69,5 +69,5 @@ function IsInjected($str)
     return false;
   }
 }
-   
+
 ?>
